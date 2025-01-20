3d9c6036baca185de54e131563a18ae744d398c6
unknown <kupzsensei@gmail.com>
2025-01-20 12:35:43 +0800

    fixed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,16 @@ function App() {
   const [menu, setMenu] = useState(false);
   return (
     <main
-      className="w-screen h-screen flex justify-center overflow-auto "
+      className="w-screen h-screen flex flex-col justify-center overflow-auto items-center gap-8"
       style={{
         backgroundImage: `url(${paperTexture})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
     >
+      <Navbar setMenu={setMenu} />
       <div className="flex-1 min-h-0 min-w-0 flex flex-col  gap-5 max-w-[1280px]">
-        <Navbar setMenu={setMenu} />
-        <Outlet context={[menu , setMenu]} />
+        <Outlet context={[menu, setMenu]} />
       </div>
     </main>
   );
